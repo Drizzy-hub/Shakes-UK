ef86f76490a4675b87a64277269f5ba81ed4d8fd
Drizzy <66376866+Drizzy-hub@users.noreply.github.com>
2024-05-26 14:57:38 +0100

    chore: added more fields

--- health/Screens/Home/Profile.jsx
+++ health/Screens/Home/Profile.jsx
@@ -1,5 +1,5 @@
-import { ScrollView, StyleSheet, View } from "react-native";
-import React, { useContext } from "react";
+import { Alert, ScrollView, StyleSheet, View } from "react-native";
+import React, { useContext, useEffect, useState } from "react";
 import useHeaderHeight from "../../hooks/getHeight";
 import Text from "../../components/Text";
 import colors from "../../components/pallets";
@@ -8,11 +8,47 @@ import Input from "../../components/Input";
 import Button from "../../components/Button";
 import { AuthenticatedUserContext } from "../../provider/authProvider";
 import Icons from "../../assets/icons/Icons";
+import { doc, setDoc, updateDoc } from "@firebase/firestore";
+import { db } from "../../firebase";
 
 const Profile = ({ navigation }) => {
   const { insets } = useHeaderHeight();
   const { userData, user } = useContext(AuthenticatedUserContext);
-  console.log(userData, "data");
+  console.log(user, "data");
+
+  const [formData, setFormData] = useState({
+    age: "",
+    emotionalStability: "",
+    employmentStatus: "",
+    financialWorry: "",
+    incomeSatisfaction: "",
+    healthServiceSatisfaction: "",
+  });
+
+  useEffect(() => {
+    setFormData({
+      age: userData.age || "",
+      emotionalStability: userData.emotionalStability || "",
+      employmentStatus: userData.employmentStatus || "",
+      financialWorry: userData.financialWorry || "",
+      incomeSatisfaction: userData.incomeSatisfaction || "",
+      healthServiceSatisfaction: userData.healthServiceSatisfaction || "",
+    });
+  }, [userData]);
+
+  const handleChange = (name, value) => {
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const saveData = async (field, value) => {
+    try {
+      await updateDoc(doc(db, "users", user.uid), { [field]: value });
+      alert("Profile Updated", "Your profile has been updated successfully.");
+    } catch (error) {
+      console.error("Error updating profile: ", error);
+      alert("Error", "There was an error updating your profile.");
+    }
+  };
   return (
     <ScrollView>
       <View>
@@ -31,35 +67,58 @@ const Profile = ({ navigation }) => {
             <Icons size={58} name={"user"} />
           </View>
           <Input
+            editable={false}
             value={userData?.name}
             style={styles.textBox}
             placeholder="Name"
             label={"First Name"}
           />
           <Input
+            editable={false}
             label={"Email"}
             value={user?.email}
             placeholder={"Email"}
             style={styles.textBox}
           />
           <Input
+            editable={false}
             label={"Phone Number"}
             value={userData?.phone}
             placeholder={"Phone Number"}
             style={styles.textBox}
           />
-          <Input label={"Age"} placeholder={"Age"} style={styles.textBox} />
           <Input
+            label={"Age"}
+            placeholder={"Age"}
+            value={formData.age}
+            onChangeText={(value) => handleChange("age", value)}
+            onBlur={() => saveData("age", formData.age)}
+            style={styles.textBox}
+          />
+          <Input
+            value={formData.emotionalStability}
+            onChangeText={(value) => handleChange("emotionalStability", value)}
+            onBlur={() =>
+              saveData("emotionalStability", formData.emotionalStability)
+            }
             label={"Emotional Stability"}
             placeholder={"Emotionally Stable"}
             style={styles.textBox}
           />
           <Input
+            value={formData.employmentStatus}
+            onChangeText={(value) => handleChange("employmentStatus", value)}
+            onBlur={() =>
+              saveData("employmentStatus", formData.employmentStatus)
+            }
             label={"What is your current employment Status?"}
             placeholder={"What is your current employment Status"}
             style={styles.textBox}
           />
           <Input
+            value={formData.financialWorry}
+            onChangeText={(value) => handleChange("financialWorry", value)}
+            onBlur={() => saveData("financialWorry", formData.financialWorry)}
             label={
               "How often do you worry about meeting your financial obligations, such as rent, utility bills, or loan repayments?"
             }
@@ -69,6 +128,11 @@ const Profile = ({ navigation }) => {
             style={styles.textBox}
           />
           <Input
+            value={formData.incomeSatisfaction}
+            onChangeText={(value) => handleChange("incomeSatisfaction", value)}
+            onBlur={() =>
+              saveData("incomeSatisfaction", formData.incomeSatisfaction)
+            }
             label={"How satisfied are you with your current level of income?"}
             placeholder={
               "How satisfied are you with your current level of income?"
@@ -76,6 +140,16 @@ const Profile = ({ navigation }) => {
             style={styles.textBox}
           />
           <Input
+            value={formData.healthServiceSatisfaction}
+            onChangeText={(value) =>
+              handleChange("healthServiceSatisfaction", value)
+            }
+            onBlur={() =>
+              saveData(
+                "healthServiceSatisfaction",
+                formData.healthServiceSatisfaction
+              )
+            }
             label={"How satisfied are you with the health service"}
             placeholder={"How satisfied are you with the health service"}
             style={styles.textBox}
